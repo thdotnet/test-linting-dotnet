999e33718e03ab08560394e7b331d0fa0a205074
Thiago Custodio <thxplode@gmail.com>
2024-07-17 16:27:25 -0500

    Update Program.cs

--- csharp-linting-example/Program.cs
+++ csharp-linting-example/Program.cs
@@ -1,5 +1,20 @@
 ﻿// Copyright © TH Corp
 
+//using System;
+//using System.Collections.Generic;
+//namespace SampleApp
+//{
+//class Program {
+//static void Main(string[] args)
+//{
+//List<string> names = new List<string>(){"John","Jane","Doe"};
+//foreach(var name in names) {Console.WriteLine(name);}
+//Console.ReadKey();
+//}
+//}
+//}
+
+
 namespace SampleApp
 {
     using System;
