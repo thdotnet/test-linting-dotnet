84fa8dfa24b53db5021bfb405262741d2e2d46f2
Thiago Custodio <thxplode@gmail.com>
2024-07-17 20:18:15 +0000

    adding code

---
+++ csharp-linting-example/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+namespace SampleApp
+{
+class Program {
+static void Main(string[] args)
+{
+List<string> names = new List<string>(){"John","Jane","Doe"};
+foreach(var name in names) {Console.WriteLine(name);}
+Console.ReadKey();
+}
+}
+}
